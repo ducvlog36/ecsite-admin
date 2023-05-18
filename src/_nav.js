c225a1df654009bb057ed8957ca294e9518c7c67
ducswan36 <anhledinhduc2596@gmail.com>
2023-05-18 23:05:36 +0900

    first commit

---
+++ src/_nav.js
@@ -0,0 +1,204 @@
+import React from 'react'
+import CIcon from '@coreui/icons-react'
+import {
+  cilBell,
+  cilCalculator,
+  cilChartPie,
+  cilCursor,
+  cilDescription,
+  cilDrop,
+  cilPeople,
+  cilSim,
+  cilBasket,
+  cilSpeedometer,
+  cilStar,
+} from '@coreui/icons'
+import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
+
+const _nav = [
+  {
+    component: CNavItem,
+    name: 'Tổng quát',
+    to: '/dashboard',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+    badge: {
+      color: 'info',
+      text: 'HOT',
+    },
+  },
+  {
+    component: CNavItem,
+    name: 'Lệnh mới',
+    to: '/theme/colors',
+    icon: <CIcon icon={cilDrop} customClassName="nav-icon" />,
+    badge: {
+      color: 'info',
+      text: 'NEW',
+    },
+  },
+  {
+    component: CNavItem,
+    name: 'Lịch sử lệnh',
+    to: '/theme/typography',
+    icon: <CIcon icon={cilSim} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Cộng tác viên',
+    to: '/theme/typography2',
+    icon: <CIcon icon={cilPeople} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: '....',
+    to: '/theme/typography1',
+    icon: <CIcon icon={cilSim} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavTitle,
+    name: 'Quản lý',
+  },
+  {
+    component: CNavGroup,
+    name: 'SV Shop',
+    to: '/base',
+    icon: <CIcon icon={cilBasket} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'Đang bán',
+        to: '/base/accordion',
+      },
+      {
+        component: CNavItem,
+        name: 'Chưa gửi',
+        to: '/base/breadcrumbs',
+      },
+      {
+        component: CNavItem,
+        name: 'Chờ thanh toán',
+        to: '/base/cards',
+      },
+      {
+        component: CNavItem,
+        name: 'Thống kê',
+        to: '/base/tables',
+      },
+      {
+        component: CNavItem,
+        name: 'Tooltips',
+        to: '/base/tooltips',
+      },
+    ],
+  },
+  {
+    component: CNavGroup,
+    name: 'Kho sim',
+    to: '/buttons',
+    icon: <CIcon icon={cilSim} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'Sim nghe gọi',
+        to: '/buttons/buttons',
+      },
+      {
+        component: CNavItem,
+        name: 'Sim data',
+        to: '/buttons/button-groups',
+      },
+      {
+        component: CNavItem,
+        name: 'Thống kê',
+        to: '/buttons/dropdowns',
+      },
+    ],
+  },
+  {
+    component: CNavGroup,
+    name: 'Cộng tác viên',
+    icon: <CIcon icon={cilPeople} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'CTV theo tháng',
+        to: '/forms/form-control',
+      },
+      {
+        component: CNavItem,
+        name: 'CTV 1 lần',
+        to: '/forms/select',
+      },
+      {
+        component: CNavItem,
+        name: 'Thống kê',
+        to: '/forms/checks-radios',
+      },
+      {
+        component: CNavItem,
+        name: 'Layout',
+        to: '/forms/layout',
+      },
+      {
+        component: CNavItem,
+        name: 'Validation',
+        to: '/forms/validation',
+      },
+    ],
+  },
+  {
+    component: CNavItem,
+    name: 'Charts',
+    to: '/charts',
+    icon: <CIcon icon={cilChartPie} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavGroup,
+    name: 'Notifications',
+    icon: <CIcon icon={cilBell} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'Alerts',
+        to: '/notifications/alerts',
+      },
+      {
+        component: CNavItem,
+        name: 'Badges',
+        to: '/notifications/badges',
+      },
+      {
+        component: CNavItem,
+        name: 'Modal',
+        to: '/notifications/modals',
+      },
+      {
+        component: CNavItem,
+        name: 'Toasts',
+        to: '/notifications/toasts',
+      },
+    ],
+  },
+  {
+    component: CNavItem,
+    name: 'Widgets',
+    to: '/widgets',
+    icon: <CIcon icon={cilCalculator} customClassName="nav-icon" />,
+    badge: {
+      color: 'info',
+      text: 'NEW',
+    },
+  },
+  {
+    component: CNavTitle,
+    name: 'Extras',
+  },
+  {
+    component: CNavItem,
+    name: 'Docs',
+    href: 'https://coreui.io/react/docs/templates/installation/',
+    icon: <CIcon icon={cilDescription} customClassName="nav-icon" />,
+  },
+]
+
+export default _nav
---
+++ src/assets/brand/logo-negative.js
@@ -0,0 +1,12 @@
+export const logoNegative = [
+  '608 134',
+  `
+  <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200">
+  <circle cx="100" cy="100" r="95" fill="none" stroke="gold" stroke-width="10" />
+  <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="40" font-family="Arial" fill="white" font-weight="bold">
+    SIMVANG
+  </text>
+</svg>
+
+`,
+]
