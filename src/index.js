a7c420155ac145b2ac80784c92bdd86ce8fd9c62
ledinhduc <anhledinhduc2596@gmail.com>
2023-05-29 21:54:45 +0900

    delete Provider

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import store from './store'
-import { Provider  } from 'react-redux'
-
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
